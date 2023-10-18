4947de1158cb518f24ba3905a1bdd832eb621646
gulshkr <gulshansinghmuzz.591@gmail.com>
2023-10-18 22:07:52 +0530

    today's python done 18th/10/2023

---
+++ chapter2.py
@@ -0,0 +1,23 @@
+# Write code that prints Hello if 1 is stored in spam, prints Howdy if 2 is stored in spam, and prints Greetings! if anything else is stored in spam.
+
+# print('Practice question 9 chapter - 2:')
+# print('Enter a number:')
+# spam = int(input())
+# if(spam == 1):
+#     print('Hello')
+# elif(spam == 2):
+#     print('Howdy')
+# else:
+#     print('Greetings!')
+
+
+# Write a short program that prints the numbers 1 to 10 using a for loop. Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+print('Practice question 13 chpter - 2')
+# for i in range(10):
+#     print(i+1)
+
+i = 0
+while i < 10:
+    print(i+1)
+    i = i+1
---
+++ helloFunc2.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print('Hello, ' + name)
+
+hello('Gulshan')
+hello('Kumar')

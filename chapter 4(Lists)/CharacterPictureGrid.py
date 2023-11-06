04fea8f2d961b42c49c7cfef685858563b4ad778
gulshkr <gulshansinghmuzz.591@gmail.com>
2023-11-06 15:32:26 +0530

    done for today

--- chapter 4(Lists)/CharacterPictureGrid.py
+++ chapter 4(Lists)/CharacterPictureGrid.py
@@ -1,15 +1,15 @@
 # Character Picture Grid
 # Say you have a list of lists where each value in the inner lists is a one-character string, like this:
 
-# grid = [['.', '.', '.', '.', '.', '.'],
-#         ['.', 'O', 'O', '.', '.', '.'],
-#         ['O', 'O', 'O', 'O', '.', '.'],
-#         ['O', 'O', 'O', 'O', 'O', '.'],
-#         ['.', 'O', 'O', 'O', 'O', 'O'],
-#         ['O', 'O', 'O', 'O', 'O', '.'],
-#         ['O', 'O', 'O', 'O', '.', '.'],
-#         ['.', 'O', 'O', '.', '.', '.'],
-#         ['.', '.', '.', '.', '.', '.']]
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
 
 # Think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. The (0, 0) origin is in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase going down.
 
@@ -26,3 +26,7 @@
 
 # Also, remember to pass the end keyword argument to print() if you don’t want a newline printed automatically after each print() call.
 
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()  # Print a newline after each column
